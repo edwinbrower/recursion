44fdfcef4bc94052c928d8fad288be72e2734bb3
Edwin Brower <edwin.brower@gmail.com>
2017-01-30 00:51:41 -0800

    finished Stringify JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,30 +3,40 @@
 
 // but you don't so you're going to write it from scratch:
 
+/*
+var script = document.createElement("script");
+script.src = "http://path/to/underscor.js";
+document.body.appendChild(script); */
+
 var stringifyJSON = function(obj) {
 	//converts numbers and booleans to string
 	if (typeof obj === "number" || typeof obj === "boolean") return ""+obj;
+	//covers null
 	if (obj === null) return "null";
+	//string-->JSON string
 	if (typeof obj === "string") return "\""+obj+"\"";
+	//array to string
 	if (Array.isArray(obj)) {
 		var stringed= obj.map(function(x){
 			return stringifyJSON(x);
 		});
 		return "["+stringed+"]"; 
 	}
+	//object to string\
 	if (typeof obj === "object") {
-		/*var stringed = obj.map(function(x){
-			return stringifyJSON(x);
-		});
-		return "{" + stringed + "}"; */
-
-		/*
-		var stringed = {};
+		var stringed = [];
 		for (var x in obj) {
-			stringifyJSON(x);
+			if (typeof(obj[x]) === 'function' || obj[x] === undefined) {continue;}
+			var res = stringifyJSON(x) + ":" +stringifyJSON(obj[x]);
+			stringed.push(res);
 		} 
-		return "{" + stringed + "}"; 
-		*/
+		return "{" + stringed + "}";
+		// might need to isEqual {} = {}. getting {,} instead of {};
+		/*var stringed = Object.keys(obj).map(function(x){
+			if (typeof obj[x] === "undefined" || typeof obj[x] === "function") {return "";}
+			return stringifyJSON(x) + ":" +stringifyJSON(obj[x]);
+		}); */
+		return "{" + stringed + "}";   
 	} 
+	return obj;
 };
-
