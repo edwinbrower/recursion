7446d430687afdcc3a156f72eb0c06b444f93384
Edwin Brower <edwin.brower@gmail.com>
2017-01-29 21:01:14 -0800

    Strinify minus objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	//converts numbers and booleans to string
+	if (typeof obj === "number" || typeof obj === "boolean") return ""+obj;
+	if (obj === null) return "null";
+	if (typeof obj === "string") return "\""+obj+"\"";
+	if (Array.isArray(obj)) {
+		var stringed= obj.map(function(x){
+			return stringifyJSON(x);
+		});
+		return "["+stringed+"]"; 
+	}
+	if (typeof obj === "object") {
+		/*var stringed = obj.map(function(x){
+			return stringifyJSON(x);
+		});
+		return "{" + stringed + "}"; */
+
+		/*
+		var stringed = {};
+		for (var x in obj) {
+			stringifyJSON(x);
+		} 
+		return "{" + stringed + "}"; 
+		*/
+	} 
 };
+
